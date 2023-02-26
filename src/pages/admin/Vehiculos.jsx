0c6144f1b7fdda7cfd1895847dd76d033e6a9fe8
Gerardo <gerardojur@gmail.com>
2023-02-26 18:10:23 -0500

    agregar estados y efectos

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -1,25 +1,58 @@
 import PrivateLayout from "layouts/PrivateLayout";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Vehiculos = () => {
-
   // La función que va a ejecutar el useEffect se coloca en el primer parámetro,
   // y va sin nombre por lo general, para ahorrar código.
   // en el array [ ] irían las variables que está escuchando para ejecutar la función,
   // pero si se deja vacío se ejecutará el useEffect una sola vez al renderizar la página
   // útil para esos casos
-  useEffect( () => {
-    console.log('Hola, soy un useEffect que se ejecuta solo una vez cuando la página se renderiza, porque tiene el array de dependencias vacío');
-  } , [ ] )
+
+  const [nombreVehiculo, setNombreVehiculo] = useState('');
+
+  useEffect(() => {
+    console.log(
+      "Hola, soy un useEffect que se ejecuta solo una vez cuando la página se renderiza, porque tiene el array de dependencias vacío"
+    );
+  }, []);
+
+  useEffect(() => {
+    console.log(
+      "Esto es una función que se ejecuta cada que cambia el valor de nombreVehiculo"
+    );
+    console.log("El valor de la variable es ", nombreVehiculo);
+  }, [nombreVehiculo]);
+
+  const enviarDatosAlBackend = () => {
+    console.log("El valor de la variable nombreVehiculo es ", nombreVehiculo);
+  };
 
   return (
     <PrivateLayout>
       <form className="flex flex-col" action="">
         <h2>Formulario de creación de vehículos</h2>
-        <input onChange={(e) =>{console.log(e.target.value)}} type="text" placeholder="Nombre del vehículo" />
-        <input type="text" placeholder="Marca del vehículo" />
+        <input
+          onChange={(e) => {
+            setNombreVehiculo(e.target.value);
+          }}
+          type="text"
+          placeholder="Nombre del vehículo"
+        />
+        <input
+          onChange={(e) => {
+            console.log(e.target.value);
+          }}
+          type="text"
+          placeholder="Marca del vehículo"
+        />
         <input type="text" placeholder="Modelo del vehículo" />
-        <button className="bg-indigo-500 text-white">Enviar datos</button>
+        <button
+          type="button"
+          onClick={enviarDatosAlBackend}
+          className="bg-indigo-500 text-white"
+        >
+          Enviar datos
+        </button>
       </form>
     </PrivateLayout>
   );
